b4e8e8c9a13fcd1519b6b21fd67118e07c384af8
Gurunath Someshwar Mule <84800721+gurunath9201@users.noreply.github.com>
2026-03-31 15:45:51 +0530

    Add CrossOrigin annotation to CorsConfig

--- WhatsAppChatBot/src/main/java/com/whatsappchatbot/config/CorsConfig.java
+++ WhatsAppChatBot/src/main/java/com/whatsappchatbot/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.whatsappchatbot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -9,6 +10,7 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin(origins = "https://whats-app-chat-bot-liard.vercel.app/")
 @Configuration
 public class CorsConfig {
 
